b976253947af754b1cb58ea9f729c209e33d2615
Gonzalo Manzaneda Burgos <gon.man.bur@gmail.com>
2024-09-07 23:30:33 -0400

    trabaje en el modulo controlador

--- controlador/loginController.php
+++ controlador/loginController.php
@@ -1,8 +1,13 @@
 <?php
+include_once '../modelo/Usuario.php'
     session_start();
     $user = $_POST['user'];
     $pass = $_POST['pass'];
-    echo $user;
-    echo " ";
-    echo $pass
-?>
+    $usuario = new Usuario();
+    $usuario->Loguearse($user,$pass);
+
+    if(!empty($usuario->objetos as $objeto))
+        foreach($usuario->objetos as $objeto) {
+            $_SESSION['usuario']=$objeto->id_usuario;
+    }
+?>    
