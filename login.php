2a44f00dc7ca811d53b58fdea53fe46d3ecb42b0
Alejandro <coroalejandro424@gmail.com>
2024-09-11 00:55:12 -0400

    cambie el nombre

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
-    <!-- hoja de estilo-->
+    <!-- hoja de estilo Alejandro-->
     <link rel="stylesheet" type="text/css" href="/webodontologia/css/fonts/fonts.css">
     <link rel="stylesheet" type="text/css" href="/webodontologia/css/style.css">
     <link rel="stylesheet" type="text/css" href="/webodontologia/css/fontawesome-free-6.6.0-web/css/all.min.css">
